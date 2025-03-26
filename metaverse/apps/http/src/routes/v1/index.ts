c0f620e5737d12b301f8b8a19eced8eb78537235
Hitanmaster <more94himanshu@gmail.com>
2025-03-26 18:20:46 +0530

    converter code

--- metaverse/apps/http/src/routes/v1/index.ts
+++ metaverse/apps/http/src/routes/v1/index.ts
@@ -2,12 +2,16 @@ import { Router } from "express";
 
 export const router = Router();
 
-router.get("/signup", () => {
-    
+router.get("/signup", (req, res) => {
+    res.json({
+        message: "Signup"
+    })
 }) 
 
 
-router.get("/signin", () => {
-    
+router.get("/signin", (req, res) => {
+    res.json({
+        message: "Signin"
+    })
 }) 
 
