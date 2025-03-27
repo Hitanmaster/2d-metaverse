6985ed485d42bfcfe1c6ec3d7331332e0ca4350a
Hitanmaster <more94himanshu@gmail.com>
2025-03-27 19:53:38 +0530

    writed the api code

--- metaverse/apps/http/src/routes/v1/index.ts
+++ metaverse/apps/http/src/routes/v1/index.ts
@@ -1,17 +1,32 @@
 import { Router } from "express";
+import { adminRouter } from "./admin";
+import { userRouter } from "./user";
+import { spaceRouter } from "./space";
 
 export const router = Router();
 
-router.get("/signup", (req, res) => {
+router.post("/signup", (req, res) => {
     res.json({
         message: "Signup"
     })
 }) 
 
 
-router.get("/signin", (req, res) => {
+router.post("/signin", (req, res) => {
     res.json({
         message: "Signin"
     })
 }) 
 
+router.get("/elements", (req, res) => {
+    
+})  
+
+router.get("/avatars", (req, res) => {
+    
+})
+
+
+router.use("/user", userRouter);
+router.use("/space", spaceRouter);
+router.use("/admin", adminRouter);
